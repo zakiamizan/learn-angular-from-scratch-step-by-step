05d33d7f9ff0abffa545077489f47205ce33f2ed
Zakia Mizan <zmizan9@gmail.com>
2023-11-05 11:22:04 -0600

    Express website, static HTML only

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'http://localhost:3000'
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
